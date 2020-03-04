467b0cbb1af964610be40b2e01ab7a60d69cc286
“B0514” <737051678@qq.com>
2020-03-04 17:07:30 +0000

    ss

--- resources/views/index/dajia.blade.php
+++ resources/views/index/dajia.blade.php
@@ -1,3 +1,5 @@
         <div style="width: 99%; height: 600px; border: 1px solid black;overflow: auto" id="list"></div>
-        <input type="text" id="message">
-        <input type="button" value="发送" id='btn'>
+        <div id="bottom">
+                <input type="text" id="txt">
+                <input type="button" value="发送" id='btn'>
+        </div>
